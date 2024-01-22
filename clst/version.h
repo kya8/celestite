d0b374bc9881c4d06af0a275dabc87ba68e13d09
Kya <46643054+kya8@users.noreply.github.com>
2024-01-22 22:32:00 +0800

    version: Add prefix to prevent name clash.

--- clst/version.h
+++ clst/version.h
@@ -1,10 +1,10 @@
 #ifndef CLST_VERSION_H
 #define CLST_VERSION_H
 
-extern const char* GIT_DESC;
-extern const char* GIT_BRANCH;
-extern const char* COMMIT_DATE;
-extern const char* TARGET_OS;
-extern const char* TARGET_ARCH;
+extern const char* CELESTITE_GIT_DESC;
+extern const char* CELESTITE_GIT_BRANCH;
+extern const char* CELESTITE_COMMIT_DATE;
+extern const char* CELESTITE_TARGET_OS;
+extern const char* CELESTITE_TARGET_ARCH;
 
 #endif /* CLST_VERSION_H */
