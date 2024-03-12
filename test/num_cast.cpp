8ed86f386650aa1b514169ef0c2a6ad9b1a43a86
Kya <46643054+kya8@users.noreply.github.com>
2024-03-13 02:37:32 +0800

    test: basic test for integral num_cast.

---
+++ test/num_cast.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include <clst/numerical/num_cast.h>
+#include <climits>
+
+using clst::num_cast;
+using clst::error::BadNumCast;
+
+TEST(NumCast, IntegralCasts)
+{
+    EXPECT_THROW(num_cast<char>(CHAR_MAX + 1), BadNumCast);
+    EXPECT_THROW(num_cast<char>(CHAR_MIN - 1), BadNumCast);
+    EXPECT_NO_THROW(EXPECT_EQ(num_cast<char>(CHAR_MAX), CHAR_MAX));
+    EXPECT_NO_THROW(EXPECT_EQ(num_cast<char>(CHAR_MIN), CHAR_MIN));
+
+    EXPECT_THROW(num_cast<int>((unsigned int)(INT_MAX + 1)), BadNumCast);
+    EXPECT_THROW(num_cast<unsigned int>(-1), BadNumCast);
+    EXPECT_NO_THROW(EXPECT_EQ(num_cast<int>((unsigned int)INT_MAX), INT_MAX));
+    EXPECT_NO_THROW(EXPECT_EQ(num_cast<int>((unsigned int)0), 0));
+    EXPECT_NO_THROW(EXPECT_EQ(num_cast<unsigned int>(INT_MAX), INT_MAX));
+}
