2cd9af4ce1f6b17a094428e893dfe740aaf1f8a9
kya <46643054+kya8@users.noreply.github.com>
2024-04-30 18:11:03 +0800

    Fix signed integer overflow

--- test/num_cast.cpp
+++ test/num_cast.cpp
@@ -12,7 +12,7 @@ TEST(NumCast, IntegralCasts)
     EXPECT_NO_THROW(EXPECT_EQ(num_cast<char>(CHAR_MAX), CHAR_MAX));
     EXPECT_NO_THROW(EXPECT_EQ(num_cast<char>(CHAR_MIN), CHAR_MIN));
 
-    EXPECT_THROW(num_cast<int>((unsigned int)(INT_MAX + 1)), BadNumCast);
+    EXPECT_THROW(num_cast<int>((unsigned int)(INT_MAX) + 1), BadNumCast);
     EXPECT_THROW(num_cast<unsigned int>(-1), BadNumCast);
     EXPECT_NO_THROW(EXPECT_EQ(num_cast<int>((unsigned int)INT_MAX), INT_MAX));
     EXPECT_NO_THROW(EXPECT_EQ(num_cast<int>((unsigned int)0), 0));
