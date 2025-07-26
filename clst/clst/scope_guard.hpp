cb0d6c0337d02b50e2a9cb8cd16612f45c22edef
kya <46643054+kya8@users.noreply.github.com>
2025-07-27 02:09:30 +0800

    Add ScopeGuard

---
+++ clst/clst/scope_guard.hpp
@@ -0,0 +1,28 @@
+#ifndef CLST_SCOPE_GUARD_HPP
+#define CLST_SCOPE_GUARD_HPP
+
+namespace clst {
+
+template<class F>
+class ScopeGuard {
+public:
+    ScopeGuard(F&& f) noexcept(noexcept(F(std::forward<F>(f)))) : func_(std::forward<F>(f)) {}
+    ~ScopeGuard() {
+        std::forward<F>(func_)();
+    }
+
+private:
+    F func_;
+};
+
+// CTAD guides
+// Store a reference for lvalue argument.
+template<typename T>
+ScopeGuard(T&) -> ScopeGuard<T&>;
+// For rvalue argument we want to move it in.
+template<typename T>
+ScopeGuard(T&&) -> ScopeGuard<T>;
+
+} // namespace clst
+
+#endif // CLST_SCOPE_GUARD_HPP
